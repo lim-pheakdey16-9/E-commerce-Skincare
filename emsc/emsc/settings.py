e2337f93b7c1c61107dde9cdd2387e3efea26360
lim-pheakdey16-9 <148685517+lim-pheakdey16-9@users.noreply.github.com>
2025-08-28 17:22:56 +0700

    fix

--- emsc/emsc/settings.py
+++ emsc/emsc/settings.py
@@ -59,10 +59,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'emsc.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
